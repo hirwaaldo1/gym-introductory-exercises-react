26dcd3d545f62d81a006acc04732fd583c4b160d
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-02-19 13:00:10 +0200

    feat: Mapping Through A List And Rendering

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import List from "./components/List";
+import DATA from "./data/data";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Learn react in the gym</p>
-      </header>
+    <div className="flex justify-center mt-2">
+      <ul className="w-fit shadow-lg rounded-md">
+        {DATA.map((value, index) => {
+          return <List name={value.name} icon={value.icon} key={index} />;
+        })}
+      </ul>
     </div>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+const data = [
+  {
+    name: "dog",
+    icon: "🐶",
+  },
+  {
+    name: "cat",
+    icon: "🐱",
+  },
+  {
+    name: "chicken",
+    icon: "🐔",
+  },
+  {
+    name: "cow",
+    icon: "🐮",
+  },
+  {
+    name: "sheep",
+    icon: "🛌",
+  },
+  {
+    name: "horse",
+    icon: "🏇",
+  },
+];
+export default data;
