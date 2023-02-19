1e93ccfe603811669b6c46eeaa17dc40afd3c815
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-02-19 12:23:21 +0200

    feat: Custom Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from "./logo.svg";
+import Button from "./components/Button";
 import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Learn react in the gym</p>
-      </header>
+    <div className="flex">
+      <div style={{ display: "flex", gap: "20px" }}>
+        {["button 1", "button 2", "button 3"].map((value, index) => {
+          return <Button title={value} msg={value} key={index} />;
+        })}
+      </div>
     </div>
   );
 }
