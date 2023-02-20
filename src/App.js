67266e2c2eab69877252f3e06ae3af92f5c00741
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-02-20 15:24:03 +0200

    fix: remove duplicates codes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 function App() {
   const [person, setPerson] = useState({ firstName: "", lastName: "" });
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setPerson({ ...person, [name]: value });
+  }
   return (
     <div className="flex justify-center">
       <div className="flex flex-col gap-3 w-1/5 mt-5">
@@ -8,19 +12,23 @@ function App() {
           type="text"
           className="input--style"
           placeholder="First name"
+          name="firstName"
           value={person.firstName}
-          onChange={(e) => setPerson({ ...person, firstName: e.target.value })}
+          onChange={(event) => handleChange(event)}
         />
         <input
           type="text"
           className="input--style"
+          name="lastName"
           value={person.lastName}
-          onChange={(e) => setPerson({ ...person, lastName: e.target.value })}
+          onChange={(event) => handleChange(event)}
           placeholder="Last name"
         />
         <button
           className="bg-rose-600 p-3 uppercase text-white font-bold  rounded-md"
           onClick={() => {
+            if (person.firstName === "" || person.lastName === "")
+              return alert("Please enter your names");
             alert(`Hello ${person.firstName} ${person.lastName}`);
             setPerson({ firstName: "", lastName: "" });
           }}
