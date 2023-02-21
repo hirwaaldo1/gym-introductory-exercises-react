d316817bdf4490a41deac2c0ea873d9a7fb22919
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-02-21 18:28:12 +0200

    fix: remove the import of react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
   return (
     <div>
       <p>Button has been clicked: {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click ME</button>
+      <button onClick={() => setCount((prev) => prev + 1)}>Click ME</button>
     </div>
   );
 }
