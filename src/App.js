3e88261c4df3ee9b2a069fe571d7cd9824a6cc61
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-02-23 10:50:20 +0200

    fix: apply all request changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div className="flex justify-center mt-2">
       <ul className="w-fit rounded-md">
-        {DATA.map((value, index) => {
-          return <List {...value} key={index} />;
-        })}
+        {DATA.map((value, index) => (
+          <List {...value} key={index} />
+        ))}
       </ul>
     </div>
   );
--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,8 @@
 import React from "react";
-export default function List({ name, id }) {
+export default function List(props) {
   return (
     <li className="flex items-center gap-3 px-4 py-5">
-      <span>{id}.</span>
-      <span>{name}</span>
+      <span>{props.name}</span>
     </li>
   );
 }
